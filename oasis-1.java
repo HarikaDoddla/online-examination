64255cdf81f6833bbe71c008e10f979035b2ca9b
HarikaDoddla <147095153+HarikaDoddla@users.noreply.github.com>
2023-11-26 18:25:35 +0530

    Update oasis-1.java

--- oasis-1.java
+++ oasis-1.java
@@ -1,38 +1,92 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
-public class Main {
+class User {
+    private String username;
+    private String password;
 
-    public static void main(String[] args) {
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
+
+class OnlineExam {
+    private Map<String, String> questions;
+
+    public OnlineExam() {
+        questions = new HashMap<>();
+        questions.put("Who designed Indian flag?", "Pingali venkayya");
+        questions.put("Which is knowns as blue planet?", "Mars");
+    }
+
+    public void startExam() {
         Scanner scanner = new Scanner(System.in);
-        
+        int score = 0;
 
-        String[] questions = {
-            "What is the capital of France?\nA. London\nB. Paris\nC. Rome\nYour answer: ",
-            "What is the largest mammal?\nA. Elephant\nB. Blue Whale\nC. Lion\nYour answer: ",
-            " What is the capital of India?\nA.DELHI\nB.MUMBAI\nc.Gujarat\n.D.ANDHRA PRADESH\n"
-            
-        };
-        
-        
-        String[] answers = {"B", "B","A"}; 
-        
-        int score = 0; 
-        
-        for (int i = 0; i < questions.length; i++) {
-            System.out.println("Question " + (i + 1) + ":");
-            System.out.println(questions[i]);
-            String userAnswer = scanner.nextLine().toUpperCase(); 
-            if (userAnswer.equals(answers[i])) {
-                System.out.println("Correct!\n");
+        System.out.println("Welcome to the Online Examination!");
+        for (Map.Entry<String, String> entry : questions.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.print("Your answer: ");
+            String userAnswer = scanner.nextLine().trim();
+
+            if (userAnswer.equalsIgnoreCase(entry.getValue())) {
+                System.out.println("Correct!");
                 score++;
             } else {
-                System.out.println("Incorrect!\n");
+                System.out.println("Incorrect!");
             }
         }
-        
-        
-        System.out.println("Quiz ended! Your score is: " + score + "/" + questions.length);
-        
+
+        System.out.println("\nExam completed! Your score: " + score + "/" + questions.size());
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, User> users = new HashMap<>();
+        users.put("user1", new User("user1", "password1"));
+        users.put("user2", new User("user2", "password2"));
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n1. Login\n2. logout");
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+
+            if (choice == 1) {
+                System.out.print("Enter username: ");
+                String username = scanner.next();
+                System.out.print("Enter password: ");
+                String password = scanner.next();
+
+                User currentUser = users.get(username);
+
+                if (currentUser != null && currentUser.getPassword().equals(password)) {
+                    System.out.println("\nLogin successful. Welcome, " + username + "!");
+                    OnlineExam exam = new OnlineExam();
+                    exam.startExam();
+                } else {
+                    System.out.println("Login failed. Please check your credentials.");
+                }
+            } else if (choice == 2) {
+                System.out.println("Quitting...");
+                break;
+            } else {
+                System.out.println("Invalid choice.");
+            }
+        }
+
         scanner.close();
     }
 }
